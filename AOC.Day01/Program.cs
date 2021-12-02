4396781cee6e4984c800d7bf371f80d69e5f93fa
Maarten Louage <me@maartenlouage.nl>
2021-12-02 09:43:40 +0100

    Part 1

--- AOC.Day01/Program.cs
+++ AOC.Day01/Program.cs
@@ -1,3 +1,15 @@
-﻿var depths = await File.ReadAllTextAsync("Data\\day01_input.txt");
+﻿var input = await File.ReadAllTextAsync("Data\\day01_input.txt");
+IList<int> depths = input.Trim().Split('\n').Select(e => int.Parse(e.Trim())).ToList();
 
-Console.WriteLine(depths.Count());
+
+int count = 0;
+
+for(int i = 1; i < depths.Count; i++)
+{
+    if (depths[i] > depths[i - 1])
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
