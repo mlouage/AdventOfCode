314e39b8c5c9af6b991a056a0b89096a71a1d29e
Maarten Louage <me@maartenlouage.nl>
2021-12-02 20:37:56 +0100

    Prepare for Day 03 and stole @mvdevries CreatePair implementation for Day 01

--- AOC.2021.Tests/Tests.cs
+++ AOC.2021.Tests/Tests.cs
@@ -17,14 +17,14 @@ public class Tests
     [Fact]
     public async Task Day01Part1Test()
     {
-        var test = await File.ReadAllTextAsync("Day01\\Data\\test.txt");
+        var test = await File.ReadAllTextAsync("Day01\\test.txt");
         var sut = new Days.Day01();
 
         var actual = sut.Part1(test);
 
         Assert.Equal(7, actual);
 
-        var input = await File.ReadAllTextAsync("Day01\\Data\\input.txt");
+        var input = await File.ReadAllTextAsync("Day01\\input.txt");
         var solution = sut.Part1(input);
 
         _output.WriteLine($"Day 01 (Part 1): {solution}");
@@ -33,14 +33,14 @@ public class Tests
     [Fact]
     public async Task Day01Part2Test()
     {
-        var test = await File.ReadAllTextAsync("Day01\\Data\\test.txt");
+        var test = await File.ReadAllTextAsync("Day01\\test.txt");
         var sut = new Days.Day01();
 
         var actual = sut.Part2(test);
 
         Assert.Equal(5, actual);
 
-        var input = await File.ReadAllTextAsync("Day01\\Data\\input.txt");
+        var input = await File.ReadAllTextAsync("Day01\\input.txt");
         var solution = sut.Part2(input);
 
         _output.WriteLine($"Day 01 (Part 2): {solution}");
@@ -49,14 +49,14 @@ public class Tests
     [Fact]
     public async Task Day02Part1Test()
     {
-        var test = await File.ReadAllTextAsync("Day02\\Data\\test.txt");
+        var test = await File.ReadAllTextAsync("Day02\\test.txt");
         var sut = new Days.Day02();
 
         var actual = sut.Part1(test);
 
         Assert.Equal(150, actual);
 
-        var input = await File.ReadAllTextAsync("Day02\\Data\\input.txt");
+        var input = await File.ReadAllTextAsync("Day02\\input.txt");
         var solution = sut.Part1(input);
 
         _output.WriteLine($"Day 02 (Part 1): {solution}");
@@ -65,16 +65,48 @@ public class Tests
     [Fact]
     public async Task Day02Part2Test()
     {
-        var test = await File.ReadAllTextAsync("Day02\\Data\\test.txt");
+        var test = await File.ReadAllTextAsync("Day02\\test.txt");
         var sut = new Days.Day02();
 
         var actual = sut.Part2(test);
 
         Assert.Equal(900, actual);
 
-        var input = await File.ReadAllTextAsync("Day02\\Data\\input.txt");
+        var input = await File.ReadAllTextAsync("Day02\\input.txt");
         var solution = sut.Part2(input);
 
         _output.WriteLine($"Day 02 (Part 2): {solution}");
+    }
+
+    [Fact]
+    public async Task Day03Part1Test()
+    {
+        var test = await File.ReadAllTextAsync("Day03\\test.txt");
+        var sut = new Days.Day03();
+
+        var actual = sut.Part1(test);
+
+        Assert.Equal(0, actual);
+
+        var input = await File.ReadAllTextAsync("Day03\\input.txt");
+        var solution = sut.Part1(input);
+
+        _output.WriteLine($"Day 03 (Part 1): {solution}");
+    }
+
+    [Fact]
+    public async Task Day03Part2Test()
+    {
+        var test = await File.ReadAllTextAsync("Day03\\test.txt");
+        var sut = new Days.Day03();
+
+        var actual = sut.Part2(test);
+
+        Assert.Equal(0, actual);
+
+        var input = await File.ReadAllTextAsync("Day03\\input.txt");
+        var solution = sut.Part2(input);
+
+        _output.WriteLine($"Day 03 (Part 2): {solution}");
     }
 }
--- AOC.2021/Days/Day01.cs
+++ AOC.2021/Days/Day01.cs
@@ -24,3 +24,32 @@ public class Day01 : IDay
                 ? total + 1 : total);
     }
 }
+
+public static class Day01Extensions
+{
+    public static IEnumerable<int> ConvertToNumbers(this string input)
+    {
+        return input.Trim().Split(Environment.NewLine).Select(n => int.Parse(n));
+    }
+
+    public static IEnumerable<int[]> CreatePair(this IEnumerable<int> input,
+        int size)
+    {
+        var queue = new Queue<int>(size);
+
+        foreach (var item in input)
+        {
+            if (queue.Count == size)
+            {
+                queue.Dequeue();
+            }
+
+            queue.Enqueue(item);
+
+            if (queue.Count == size)
+            {
+                yield return queue.ToArray();
+            }
+        }
+    }
+}
