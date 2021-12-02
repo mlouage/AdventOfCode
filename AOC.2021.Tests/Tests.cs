d177cb7e0d168e40728d900126b38a9af5acc545
Maarten Louage <me@maartenlouage.nl>
2021-12-02 16:53:56 +0100

    Prepare for day 02

---
+++ AOC.2021.Tests/Tests.cs
@@ -0,0 +1,80 @@
+﻿using Xunit;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace AOC._2021.Tests;
+
+public class Tests
+{
+    private readonly ITestOutputHelper _output;
+
+    public Tests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public async Task Day01Part1Test()
+    {
+        var test = await File.ReadAllTextAsync("Day01\\Data\\test.txt");
+        var sut = new Days.Day01();
+
+        var actual = sut.Part1(test);
+
+        Assert.Equal(7, actual);
+
+        var input = await File.ReadAllTextAsync("Day01\\Data\\input.txt");
+        var solution = sut.Part1(input);
+
+        _output.WriteLine($"Day 01 (Part 1): {solution}");
+    }
+
+    [Fact]
+    public async Task Day01Part2Test()
+    {
+        var test = await File.ReadAllTextAsync("Day01\\Data\\test.txt");
+        var sut = new Days.Day01();
+
+        var actual = sut.Part2(test);
+
+        Assert.Equal(5, actual);
+
+        var input = await File.ReadAllTextAsync("Day01\\Data\\input.txt");
+        var solution = sut.Part2(input);
+
+        _output.WriteLine($"Day 01 (Part 2): {solution}");
+    }
+
+    [Fact]
+    public async Task Day02Part1Test()
+    {
+        var test = await File.ReadAllTextAsync("Day02\\Data\\test.txt");
+        var sut = new Days.Day02();
+
+        var actual = sut.Part1(test);
+
+        Assert.Equal(7, actual);
+
+        var input = await File.ReadAllTextAsync("Day02\\Data\\input.txt");
+        var solution = sut.Part1(input);
+
+        _output.WriteLine($"Day 02 (Part 1): {solution}");
+    }
+
+    [Fact]
+    public async Task Day02Part2Test()
+    {
+        var test = await File.ReadAllTextAsync("Day02\\Data\\test.txt");
+        var sut = new Days.Day02();
+
+        var actual = sut.Part2(test);
+
+        Assert.Equal(5, actual);
+
+        var input = await File.ReadAllTextAsync("Day02\\Data\\input.txt");
+        var solution = sut.Part2(input);
+
+        _output.WriteLine($"Day 02 (Part 2): {solution}");
+    }
+}
---
+++ AOC.Day01/Days/Day02.cs
@@ -0,0 +1,13 @@
+﻿namespace AOC._2021.Days;
+public class Day02 : IDay
+{
+    public int Part1(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Part2(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
