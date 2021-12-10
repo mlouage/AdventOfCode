24e3b2a99e8be5bf85b1f0e7b82f62cd3571f10d
Maarten Louage <me@maartenlouage.nl>
2021-12-10 10:56:18 +0100

    Day 10

--- AOC.2021.Tests/Tests.cs
+++ AOC.2021.Tests/Tests.cs
@@ -332,4 +332,36 @@ public class Tests
 
         _output.WriteLine($"Day 09 (Part 2): {solution}");
     }
+
+    [Fact]
+    public async Task Day10Part1Test()
+    {
+        var test = await File.ReadAllLinesAsync("Day10\\test.txt");
+        var sut = new Days.Day10();
+
+        var actual = sut.Part1(test);
+
+        Assert.Equal(26397, actual);
+
+        var input = await File.ReadAllLinesAsync("Day10\\input.txt");
+        var solution = sut.Part1(input);
+
+        _output.WriteLine($"Day 10 (Part 1): {solution}");
+    }
+
+    [Fact]
+    public async Task Day10Part2Test()
+    {
+        var test = await File.ReadAllLinesAsync("Day10\\test.txt");
+        var sut = new Days.Day10();
+
+        var actual = sut.Part2(test);
+
+        Assert.Equal(288957, actual);
+
+        var input = await File.ReadAllLinesAsync("Day10\\input.txt");
+        var solution = sut.Part2(input);
+
+        _output.WriteLine($"Day 10 (Part 2): {solution}");
+    }
 }
---
+++ AOC.2021/Days/Day10.cs
@@ -0,0 +1,162 @@
+﻿namespace AOC._2021.Days;
+
+public class Day10
+{
+    public int Part1(string[] input)
+    {
+        var chunks = new Dictionary<char, char>()
+        {
+            { '(', ')' },
+            { '[', ']' },
+            { '{', '}' },
+            { '<', '>' }
+        };
+        var openingChunks = new char[]{ '(', '[', '{', '<' };
+        var result = new Queue<char>();
+        var corrupted = false;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            var closingStack = new Stack<char>();
+            var line = input[i];
+
+            for (int j = 0; j < line.Length; j++)
+            {
+                var chunk = line[j];
+                var validNextChunks = new List<char>();
+                
+                if (openingChunks.Contains(chunk))
+                {
+                    var correspondingClosingChunk = chunks[chunk];
+                    closingStack.Push(correspondingClosingChunk);
+                    
+                    validNextChunks.AddRange(openingChunks);
+                    validNextChunks.Add(correspondingClosingChunk);
+                }
+                else
+                {                    
+                    var validClosingChunk = closingStack.Pop();
+                    validNextChunks.Add(validClosingChunk);
+                    validNextChunks.AddRange(openingChunks);
+                }
+                
+                var next = line[j];
+
+                if (validNextChunks.Contains(next))
+                {
+                    continue;
+                }
+                else
+                {
+                    corrupted = true;
+                    result.Enqueue(chunk);
+                    break;
+                }
+            }
+
+            if (corrupted)
+            {                
+                corrupted = false;
+            }
+        }
+
+        var sum = 0;
+
+        while(result.Any())
+        {
+            var item = result.Dequeue();
+
+            if (item == ')') sum += 3;
+            if (item == ']') sum += 57;
+            if (item == '}') sum += 1197;
+            if (item == '>') sum += 25137;
+        }
+
+        return sum;
+    }
+
+    public long Part2(string[] input)
+    {
+        var chunks = new Dictionary<char, char>()
+        {
+            { '(', ')' },
+            { '[', ']' },
+            { '{', '}' },
+            { '<', '>' }
+        };
+        var openingChunks = new char[] { '(', '[', '{', '<' };
+        var result = new Queue<char>();
+        var corrupted = false;
+        var lineSums = new List<long>();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            var closingStack = new Stack<char>();
+            var line = input[i];
+
+            for (int j = 0; j < line.Length; j++)
+            {
+                var chunk = line[j];
+                var validNextChunks = new List<char>();
+
+                if (openingChunks.Contains(chunk))
+                {
+                    var correspondingClosingChunk = chunks[chunk];
+                    closingStack.Push(correspondingClosingChunk);
+
+                    validNextChunks.AddRange(openingChunks);
+                    validNextChunks.Add(correspondingClosingChunk);
+                }
+                else
+                {
+                    var validClosingChunk = closingStack.Pop();
+                    validNextChunks.Add(validClosingChunk);
+                    validNextChunks.AddRange(openingChunks);
+                }
+
+                var next = line[j];
+
+                if (validNextChunks.Contains(next))
+                {
+                    continue;
+                }
+                else
+                {
+                    corrupted = true;
+                    result.Enqueue(chunk);
+                    break;
+                }
+            }
+
+            if (corrupted)
+            {
+                corrupted = false;
+            }
+            else
+            {
+                var lineSum = 0L;
+
+                while (closingStack.Any())
+                {
+                    var item = closingStack.Pop();
+                    var points = 0;
+
+                    if (item == ')') points = 1;
+                    if (item == ']') points = 2;
+                    if (item == '}') points = 3;
+                    if (item == '>') points = 4;
+
+                    lineSum = lineSum * 5 + points;
+                }
+
+                lineSums.Add(lineSum);
+            }
+        }
+
+        var orderedLineSums = lineSums.OrderBy(x => x).ToList();
+        var index = (orderedLineSums.Count / 2);
+
+        return orderedLineSums[index];
+    }
+}
+
