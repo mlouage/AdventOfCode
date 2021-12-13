1979095326723e22ba87d168e510bd1c24c6c74e
Maarten Louage <me@maartenlouage.nl>
2021-12-13 17:12:26 +0100

    Tests

--- AOC.2021.Tests/Tests.cs
+++ AOC.2021.Tests/Tests.cs
@@ -428,4 +428,20 @@ public class Tests
 
         _output.WriteLine($"Day 12 (Part 2): {solution}");
     }
+
+    [Fact]
+    public async Task Day13Part1Test()
+    {
+        var test = await File.ReadAllLinesAsync("Day13\\test.txt");
+        var sut = new Days.Day13();
+
+        var actual = sut.Part1(test);
+
+        Assert.Equal(17, actual);
+
+        var input = await File.ReadAllLinesAsync("Day13\\input.txt");
+        var solution = sut.Part1(input);
+
+        _output.WriteLine($"Day 13 (Part 1): {solution}");
+    }
 }
